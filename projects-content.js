3f7493ff04aa2f4dcaf4d6c73d4e3ce1f9877dad
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-08-07 14:35:43 +1200

    create projects content file

---
+++ projects-content.js
@@ -0,0 +1,29 @@
+export default projects = [
+  {
+    title: 'E-commerce app',
+    description:
+      "Bnanas is an e-commerce app that sells exotic bananas. We created this app because we wanted to learn how e-commerce works and get practical experience. While working on this project, we faced some challenges and learned a lot. One thing I realized is that we needed to communicate and work together better as a team. If we had clearer roles and knew who was responsible for what, it would have made things go smoother. Overall, I am very happy with how this project turned out and I'm excited for you to check it out!",
+    technologies: 'React, Typescript, SQLite, Vite, HTML, CSS',
+    githubLink: 'https://github.com/daniel-grabczewski/bnanas',
+    image:
+      'https://raw.githubusercontent.com/daniel-grabczewski/portfolio/636355db4262d6c1a7412d796c98eae859da4869/uploads/project-bnana-cover.jpg',
+  },
+  {
+    title: 'Todo list',
+    description:
+      'This Todo List project aimed to provide practical experience in task management. A challenge we encountered was creating an intuitive user interface that streamlined task organization. We focused on designing a system that allowed users to prioritize tasks, set priorities, and filter their to-do items effectively. Overall, we are proud of the outcome of our Todo List project and excited for you to explore it. It allowed us to gain insights into task management systems while refining our skills. Give it a try and discover how it can enhance your productivity and organization!',
+    technologies: 'React, Typescript, SQLite, HTML, CSS',
+    githubLink: 'https://github.com/daniel-grabczewski/todo-full-stack',
+    image:
+      'https://raw.githubusercontent.com/daniel-grabczewski/portfolio/3637ce6cc180f0ef6dc11f2934acae73e07c188e/uploads/project-todo-cover.jpg',
+  },
+  {
+    title: 'Painting app',
+    description:
+      'PaintMyMuseic is an exciting web app that blends the joy of painting with the magic of music. Our motivation behind this project was to explore the fusion of colors and melodies, creating a unique artistic experience through the lens of web development. While painting with each color, a song will play specific to that color! Throughout our journey, we encountered challenges that helped us grow. One valuable lesson we learned was the importance of optimizing performance to ensure smooth interaction between painting and music. By improving our code and using efficient algorithms, we enhanced the overall user experience. To conclude, we are thrilled with how PaintByMuseic turned out and we invite you to experience it for yourself.',
+    technologies: 'React, Typescript, HTML, CSS',
+    githubLink: 'https://github.com/daniel-grabczewski/paint-my-museic',
+    image:
+      'https://raw.githubusercontent.com/daniel-grabczewski/portfolio/4627229b6a3c188b86bc93e66071c10edffea23f/uploads/project-paint-my-museic-cover.jpg',
+  },
+]
