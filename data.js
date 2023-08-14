8f9d8308254c693b587b5c813901eb1b2fd9f89c
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-08-14 15:34:01 +1200

    update bnana project

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 var projects = [
   {
-    title: 'E-commerce app',
+    title: 'Bnana',
     description:
       "Bnanas is an e-commerce app that sells exotic bananas. We created this app because we wanted to learn how e-commerce works and get practical experience. While working on this project, we faced some challenges and learned a lot. One thing I realized is that we needed to communicate and work together better as a team. If we had clearer roles and knew who was responsible for what, it would have made things go smoother. Overall, I am very happy with how this project turned out and I'm excited for you to check it out!",
     technologies: 'React, Typescript, SQLite, Vite, HTML, CSS',
@@ -18,9 +18,9 @@ var projects = [
       'https://raw.githubusercontent.com/daniel-grabczewski/daniel-grabczewski.github.io/02cd502b927bd034af7a92f9a67158d455c917c6/uploads/project-todo-cover.jpg',
   },
   {
-    title: 'Painting app',
+    title: 'Paint My Museic',
     description:
-      'PaintMyMuseic is an exciting web app that blends the joy of painting with the magic of music. Our motivation behind this project was to explore the fusion of colors and melodies, creating a unique artistic experience through the lens of web development. While painting with each color, a song will play specific to that color! Throughout our journey, we encountered challenges that helped us grow. One valuable lesson we learned was the importance of optimizing performance to ensure smooth interaction between painting and music. By improving our code and using efficient algorithms, we enhanced the overall user experience. To conclude, we are thrilled with how PaintByMuseic turned out and we invite you to experience it for yourself.',
+      'Paint My Museic is an exciting web app that blends the joy of painting with the magic of music. Our motivation behind this project was to explore the fusion of colors and melodies, creating a unique artistic experience through the lens of web development. While painting with each color, a song will play specific to that color! Throughout our journey, we encountered challenges that helped us grow. One valuable lesson we learned was the importance of optimizing performance to ensure smooth interaction between painting and music. By improving our code and using efficient algorithms, we enhanced the overall user experience. To conclude, we are thrilled with how PaintByMuseic turned out and we invite you to experience it for yourself.',
     technologies: 'React, Typescript, HTML, CSS',
     githubLink: 'https://github.com/daniel-grabczewski/paint-my-museic',
     image:
