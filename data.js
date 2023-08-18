a92a3247d6142c2fbcc5f07e15da68113a743538
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-08-18 17:05:33 +1200

    reduce 'reduce project descriptions content' section

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ var projects = [
   {
     title: 'Bnana',
     description:
-      "Bnanas is an e-commerce app that sells exotic bananas. We created this app because we wanted to learn how e-commerce works and get practical experience. While working on this project, we faced some challenges and learned a lot. One thing I realized is that we needed to communicate and work together better as a team. If we had clearer roles and knew who was responsible for what, it would have made things go smoother. Overall, I am very happy with how this project turned out and I'm excited for you to check it out!",
+      "Bnanas is an e-commerce app that sells exotic bananas. We created this app because we wanted to learn how e-commerce works and get practical experience. While working on this project, we faced some challenges and learned a lot. One thing I realized is that we needed to communicate and work more efficiently as a team. If we had clearer roles and knew who was responsible for what, it would have made things go smoother. Overall, I am very happy with how this project turned out and I'm excited for you to check it out!",
     technologies: 'React, Typescript, SQLite, Vite, HTML, CSS',
     githubLink: 'https://github.com/daniel-grabczewski/bnanas',
     image:
@@ -11,7 +11,7 @@ var projects = [
   {
     title: 'Boba Buddies',
     description:
-      "Boba Buddies is an e-commerce platform that brings bubble tea lovers a world of flavors. During the development, we faced a challenge with syncing the front-end with SQLite to ensure real-time inventory updates. Through collaboration and iterative testing with Jest, we streamlined the data flow, creating a responsive and efficient user experience. This project taught us the importance of adaptability and alignment between different layers of the application. I'm thrilled with the final result, and can't wait for you to explore Boba Buddies for yourself.",
+      "Boba Buddies is an e-commerce platform that brings bubble tea lovers a world of flavors. During the development, we faced a challenge with syncing the front-end with SQLite to ensure real-time inventory updates. Through collaboration and iterative testing with Jest, we streamlined the data flow, creating a responsive and efficient user experience. This project taught us the importance of adaptability and alignment between different layers of the application.",
     technologies: 'React, Typescript, SQLite, Jest, HTML, CSS',
     githubLink: 'https://github.com/Boba-Buddies/boba-buddies-store',
     image:
@@ -20,7 +20,7 @@ var projects = [
   {
     title: 'Todo list',
     description:
-      'This Todo List project aimed to provide practical experience in task management. A challenge we encountered was creating an intuitive user interface that streamlined task organization. We focused on designing a system that allowed users to prioritize tasks, set priorities, and filter their to-do items effectively. Overall, we are proud of the outcome of our Todo List project and excited for you to explore it. It allowed us to gain insights into task management systems while refining our skills. Give it a try and discover how it can enhance your productivity and organization!',
+      'This Todo List project aimed to provide practical experience in task management. A challenge we encountered was creating an intuitive user interface that streamlined task organization. We focused on designing a system that allowed users to prioritize tasks, set priorities, and filter their to-do items effectively. Overall, we are proud of the outcome of our Todo List project and excited for you to explore it. It allowed us to gain insights into task management systems while refining our skills.',
     technologies: 'React, Redux, Typescript, SQLite, HTML, CSS',
     githubLink: 'https://github.com/daniel-grabczewski/todo-full-stack',
     image:
@@ -29,7 +29,7 @@ var projects = [
   {
     title: 'Paint My Museic',
     description:
-      'Paint My Museic is an exciting web app that blends the joy of painting with the magic of music. Our motivation behind this project was to explore the fusion of colors and melodies, creating a unique artistic experience through the lens of web development. While painting with each color, a song will play specific to that color! Throughout our journey, we encountered challenges that helped us grow. One valuable lesson we learned was the importance of optimizing performance to ensure smooth interaction between painting and music. By improving our code and using efficient algorithms, we enhanced the overall user experience. To conclude, we are thrilled with how PaintByMuseic turned out and we invite you to experience it for yourself.',
+      'Paint My Museic is an exciting web app that blends the joy of painting with the magic of music. My motivation behind this project was to explore the fusion of colors and melodies, creating a unique artistic experience; while painting with each color, a song will play specific to that color! Throughout our journey, I encountered challenges that helped me grow. One valuable lesson I learned was the importance of optimizing performance to ensure smooth interaction between painting and music.',
     technologies: 'React, Typescript, HTML, CSS',
     githubLink: 'https://github.com/daniel-grabczewski/paint-my-museic',
     image:
