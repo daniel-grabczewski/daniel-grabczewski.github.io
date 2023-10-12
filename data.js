9838181970d30d70610e694db7687895649b808b
Daniel Grabczewski <danielgrabczewski@gmail.com>
2023-10-13 10:09:33 +1300

    replace boba buddies with boba business management

--- data.js
+++ data.js
@@ -8,10 +8,21 @@ var projects = [
     image:
       'https://raw.githubusercontent.com/daniel-grabczewski/daniel-grabczewski.github.io/8f9d8308254c693b587b5c813901eb1b2fd9f89c/uploads/project-bnana-cover.jpg',
   },
+  /*
   {
     title: 'Boba Buddies (80% Complete)',
     description:
       'Boba Buddies is an e-commerce platform that brings bubble tea lovers a world of flavors. During the development, we faced a challenge with syncing the front-end with SQLite to ensure real-time inventory updates. Through collaboration and iterative testing with Jest, we streamlined the data flow, creating a responsive and efficient user experience. This project taught us the importance of adaptability and alignment between different layers of the application.',
+    technologies: 'React, Typescript, SQLite, Jest, HTML, CSS',
+    githubLink: 'https://github.com/Boba-Buddies/boba-buddies-store',
+    image:
+      'https://raw.githubusercontent.com/daniel-grabczewski/daniel-grabczewski.github.io/d10d2203a572d61d099b42083313bf073725c9e2/uploads/project-boba-buddies-cover.jpg',
+  },
+  */
+  {
+    title: 'Boba Business Management',
+    description:
+      'Boba Business Management is an Business Management Platform that allows authenticated users to modify &add products, manage stock levels, view orders, read emails, and moderate user reviews. During the development, we faced a challenge with syncing the front-end with SQLite to ensure real-time inventory updates. Through collaboration and iterative testing with Jest, we streamlined the data flow, creating a responsive and efficient user experience. This project taught us the importance of adaptability and alignment between different layers of the application.',
     technologies: 'React, Typescript, SQLite, Jest, HTML, CSS',
     githubLink: 'https://github.com/Boba-Buddies/boba-buddies-store',
     image:
