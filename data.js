50d5de0b7cfd4fe4027092728520616371ae7590
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-01-08 12:35:49 +1300

    add demo links to array

--- data.js
+++ data.js
@@ -5,6 +5,7 @@ var projects = [
       'Boba Business Management is a Business Management Platform that allows authenticated users to modify & add products, manage stock levels, view orders, read emails, and moderate user reviews. During the development, we faced a challenge with syncing the front-end with SQLite to ensure real-time inventory updates. Through collaboration and iterative testing with Jest, we streamlined the data flow, creating a responsive and efficient user experience. This project taught us the importance of adaptability and alignment between different layers of the application.',
     technologies: 'React, Typescript, SQLite, Jest, Auth0, CSS',
     githubLink: 'https://github.com/Boba-Buddies/boba-buddies-store',
+    demoLink: '',
     image:
       'https://raw.githubusercontent.com/daniel-grabczewski/daniel-grabczewski.github.io/9852f9d4f4073b87bac83ad4dd06947be43cb26d/uploads/project-boba-business-management-cover.png',
   },
@@ -14,6 +15,7 @@ var projects = [
       'Bnanas is an e-commerce app that sells exotic bananas. We created this app because we wanted to learn how e-commerce works and get practical experience. While working on this project, we learned a lot about delegating roles and playing to our strengths. But at the same time, we made sure we were all actively collaborating and communicating. Through combining these two philosophies, we were able to work productively and effectively.',
     technologies: 'React, Typescript, SQLite, Vite, Auth0, CSS',
     githubLink: 'https://github.com/daniel-grabczewski/bnanas',
+    demoLink: 'https://daniel-grabczewski.github.io/bnanas-demo/',
     image:
       'https://raw.githubusercontent.com/daniel-grabczewski/daniel-grabczewski.github.io/8f9d8308254c693b587b5c813901eb1b2fd9f89c/uploads/project-bnana-cover.jpg',
   },
@@ -34,6 +36,7 @@ var projects = [
       'Paint My Museic is an exciting web app that blends the joy of painting with the magic of music. My motivation behind this project was to explore the fusion of colors and melodies, creating a unique artistic experience; while painting with each color, a song will play specific to that color! Throughout our journey, I encountered challenges that helped me grow. One valuable lesson I learned was the importance of optimizing performance to ensure smooth interaction between painting and music.',
     technologies: 'React, Typescript, HTML, CSS',
     githubLink: 'https://github.com/daniel-grabczewski/paint-my-museic',
+    demoLink: 'https://daniel-grabczewski.github.io/paint-my-museic-v2/',
     image:
       'https://raw.githubusercontent.com/daniel-grabczewski/daniel-grabczewski.github.io/8b3d051884e29ddbd74f4359f0d3536eb1440370/uploads/project-paint-my-museic-cover.svg',
   },
@@ -43,6 +46,7 @@ var projects = [
       'This Todo List project aimed to turn a typical task management app into an ethereal experience, allowing you to create todos while being one with the cosmos. A challenge I encountered was creating the aurora borealis that was always morphing in the background. After much trial and error, and encountering a lot of performance issues, I combined GSAP (Greensock) and Granim to create butter-smooth animations. Overall, I am proud of the outcome of this mystical Todo List project and excited for you to check it out.',
     technologies: 'React, Redux, Typescript, SQLite, GSAP, Granim, CSS',
     githubLink: 'https://github.com/daniel-grabczewski/todos-for-the-cosmos',
+    demoLink: 'https://daniel-grabczewski.github.io/todos-for-the-cosmos-demo/',
     image:
       'https://raw.githubusercontent.com/daniel-grabczewski/daniel-grabczewski.github.io/9852f9d4f4073b87bac83ad4dd06947be43cb26d/uploads/project-todo-cover.svg',
   },
