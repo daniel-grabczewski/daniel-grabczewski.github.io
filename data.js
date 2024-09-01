86ca1278127a2d33b9b7fea45354483fdb226fba
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-09-02 09:52:16 +1200

    Change link of BBM to navigate user to the admin dashboard page
    This first impression will help convey that this is primarily a business management app, rather than just another store

--- data.js
+++ data.js
@@ -1,18 +1,20 @@
 var projects = [
   {
     title: 'Boba Business Management',
-    name : 'boba-business-management',
+    name: 'boba-business-management',
     description:
       'Boba Business Management is a Business Management Platform that allows users to modify & add products, manage stock levels, view orders, read emails, and moderate user reviews. And also switch to being a shopper!',
     technologies: 'React, Typescript, SQLite, Jest, Auth0, CSS',
-    githubLink: 'https://github.com/daniel-grabczewski/boba-business-management',
-    demoLink: 'https://daniel-grabczewski.github.io/boba-business-management/',
+    githubLink:
+      'https://github.com/daniel-grabczewski/boba-business-management',
+    demoLink:
+      'https://daniel-grabczewski.github.io/boba-business-management/admin',
     image:
       'https://raw.githubusercontent.com/daniel-grabczewski/daniel-grabczewski.github.io/9852f9d4f4073b87bac83ad4dd06947be43cb26d/uploads/project-boba-business-management-cover.png',
   },
   {
     title: 'Bnana Online Fruit Store',
-    name : 'bnana',
+    name: 'bnana',
     description:
       'Bnana is an e-commerce app that sells exotic bananas. Users can create an account, add products to their cart, modify quantities, select shipping options, and go through the checkout process.',
     technologies: 'React, Typescript, SQLite, Vite, Auth0, CSS',
@@ -35,7 +37,7 @@ var projects = [
   */
   {
     title: 'Paint My Museic',
-    name : 'paint-my-museic',
+    name: 'paint-my-museic',
     description:
       'Paint My Museic is an exciting web app that blends the joy of painting with the magic of music, creating a unique artistic experience - while painting with each colour, a song will play specific to that colour!',
     technologies: 'React, Typescript, HTML, CSS',
@@ -46,7 +48,7 @@ var projects = [
   },
   {
     title: 'Todos For The Cosmos',
-    name : 'todos-for-the-cosmos',
+    name: 'todos-for-the-cosmos',
     description:
       'This Todo List project aimed to turn a typical task management app into an ethereal experience, allowing you to create todos while being one with the cosmos, through a breathtaking UI and animations.',
     technologies: 'React, Redux, Typescript, SQLite, GSAP, Granim, CSS',
